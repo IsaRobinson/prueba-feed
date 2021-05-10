0a4e0c436e13f68dfe6bc761ae498e1e5293a2ed
IsaRobinson <is.robinson@duocud.cl>
2021-05-09 20:33:56 -0400

    update

---
+++ instaFeed.js
@@ -0,0 +1,7 @@
+var userFeed = new Instafeed({
+    get: 'user',
+    target: "instafeed-container",
+    resolution: 'low_resolution',
+    accessToken: 'IGQVJXVVM5T2RiTmdaWFhLMGRWN0NQUTBMZAGlKeGhXWUdrR0hQVnM5RTdiSGp1NE9aR2I3NGd6djZAlYTNYZAkRXSk4zTTFsakdQQklTeFlValZAkOWtyUE9uZAXUxdE4xSXlMME92MFNoVWV3eG9EaWJGQgZDZD'
+});
+userFeed.run();
